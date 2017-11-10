eb5b0f3f477a6514a356ff9c63fbfbaa04d8e8f7
Chad Garrett <chadgarrett_@hotmail.com>
2017-11-09 23:56:33 -0800

    Refactored index code

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,17 @@
-require('dotenv').config();
-
-const MongoDB = require('mongodb');
-const cookieParser = require('cookie-parser');
+// Import bodyParser library 
 const bodyParser = require('body-parser');
+// Import Firebase Functions Library
 const functions = require('firebase-functions');
+// Import Express Server Library and initialize
+const express = require('express');
+const app = express();
 
-var path = require('path');
-var express = require('express');
-var app = express();
-var router = express.Router();
-var server = require('http').createServer(app);
-var mongoose = require('mongoose');
+// Apply bodyParser middleware to parse incoming JSON data to req.body
+app.use(bodyParser.urlencoded({ extended: true }))
+   .use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
-var db = mongoose.connection;
+// Use Express Router
+app.use('/', require('./routes/api'));
 
-db.on('error', function () {
-    console.log('Connection error to MongoDB')
-});
-
-db.once('open', function () {
-    console.log('MongoDB Connected');
-});
-
-// using webpack-dev-server and middleware in development environment
-if (process.env.NODE_ENV !== 'production') {
-    var webpackDevMiddleware = require('webpack-dev-middleware');
-    var webpackHotMiddleware = require('webpack-hot-middleware');
-    var webpack = require('webpack');
-    var config = require('./webpack.config');
-    var compiler = webpack(config);
-    app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-    app.use(webpackHotMiddleware(compiler));
-}
-
-app.use(express.static(path.join(__dirname, 'public')))
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use(bodyParser.json())
-    .use(cookieParser());
-
-app.get('/api', function(req, res) {
-    res.send('api');
-});
-
-app.get('/api/projects', function(req, res) {
-    
-    res.send('api-projects');
-});
-
-app.use('/api-docs', function(req, res) {
-    res.send('api-docs');
-});
-
-
+// Export functions as app to Firebase
 exports.app = functions.https.onRequest(app);
